d6161968f7382d51ae3d581242b0ec67c4aee7d5
Athanasios Kostopoulos <lixtetrax@grhack.net>
2015-04-09 07:39:51 +0000

    first usable version

--- scripts/freebsd.sh
+++ scripts/freebsd.sh
@@ -2,42 +2,32 @@
 set -eu
 
 GO_PACKAGE="go1.4.1.freebsd-amd64.tar.gz"
-
+MAGIC=$(pwd)
 echo *** Installing dependencies
 sudo pkg install -y git
-
 echo *** Changing to temp directory
 cd /tmp
-BUILD_DIR=go-build.$$
-mkdir $BUILD_DIR
-cd $BUILD_DIR
+mkdir build.$$
+cd build.$$
+echo *** Installing bats
+git clone https://github.com/sstephenson/bats.git
+cd bats
+sudo ./install.sh /usr/local
+cd ..
 echo *** Downloading and installing Go
 test -e $GO_PACKAGE || curl -sO https://storage.googleapis.com/golang/$GO_PACKAGE
-sudo tar -C /usr/local -xzf $GO_PACKAGE
-export PATH=$PATH:/usr/local/go/bin
-
-echo *** Changing to go project directory
+tar -C /usr/local -xzf $GO_PACKAGE
+echo *** Setting up the go project directory
 mkdir -p go/src
 cd go
-export GOPATH=`pwd`
-
-echo *** Getting libraries
-go get "github.com/docopt/docopt-go"
-go get "github.com/xeipuuv/gojsonschema"
-go get "github.com/BurntSushi/toml"
-
+export GOPATH=$(pwd)
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+go get github.com/mattn/gom
 echo *** Cloning packages
 cd src
-test -d core || git clone https://github.com/pki-io/core.git
 test -d admin || git clone https://github.com/pki-io/admin.git
-
-echo *** Building packages
-cd core
-make
-cd ..
+echo *** Building binaries
 cd admin
-make # && make install
-cd ..
-
-echo *** Done
-exit 0
+make
+test -e pki.io && cp pki.io $MAGIC
+rm -fr /tmp/build.$$
